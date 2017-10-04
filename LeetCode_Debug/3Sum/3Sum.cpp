06e6caf1e46699ec8713f1c501fec9e06cfbba0b
reilaky <reilaky@outlook.com>
2017-10-04 11:25:29 -0700

    initializere repo

---
+++ LeetCode_Debug/3Sum/3Sum.cpp
@@ -0,0 +1,9 @@
+//
+//  3Sum.cpp
+//  LeetCode_Debug
+//
+//  Created by Yan Kong on 10/4/17.
+//  Copyright © 2017 Yan Kong. All rights reserved.
+//
+
+#include "3Sum.hpp"
---
+++ LeetCode_Debug/3Sum/3Sum.hpp
@@ -0,0 +1,14 @@
+//
+//  3Sum.hpp
+//  LeetCode_Debug
+//
+//  Created by Yan Kong on 10/4/17.
+//  Copyright © 2017 Yan Kong. All rights reserved.
+//
+
+#ifndef _Sum_hpp
+#define _Sum_hpp
+
+#include <stdio.h>
+
+#endif /* _Sum_hpp */
